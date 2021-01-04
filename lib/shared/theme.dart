c0bd11eab4e9bfa456c0182ff7602ffb01d9c594
montohang <harmonsitohang17@gmail.com>
2021-01-04 20:38:28 +0700

    [ADD Sign In Page]

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -4,5 +4,10 @@ Color mainColor = "FFC700".toColor();
 Color greyColor = "8D92A3".toColor();
 
 TextStyle greyFontSytle = GoogleFonts.poppins().copyWith(color: greyColor);
+TextStyle blackFontSytle1 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w500);
+TextStyle blackFontSytle2 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w500);
+TextStyle blackFontSytle3 = GoogleFonts.poppins().copyWith(color: Colors.black);
 
 const double defaultMargin = 24;
