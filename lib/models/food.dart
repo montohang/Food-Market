eb4cc69b9a654552f43d08355332acf7fe685de7
montohang <harmonsitohang17@gmail.com>
2021-01-07 01:45:50 +0700

    [MODIFY Food Page and Food Card]

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -19,13 +19,60 @@ class Food {
       this.rate});
 }
 
-Food mockFood = Food(
-    id: 1,
-    picturePath: "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
-    name: "Sate Padang",
-    description:
-        "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
-    ingredients:
-        "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
-    price: 200000,
-    rate: 4.8);
+List<Food> mockFoods = [
+  Food(
+      id: 1,
+      picturePath:
+          "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+      name: "Sate Padang",
+      description:
+          "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+      ingredients:
+          "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+      price: 200000,
+      rate: 4.8),
+  Food(
+      id: 2,
+      picturePath:
+          "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+      name: "Sate Padang",
+      description:
+          "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+      ingredients:
+          "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+      price: 200000,
+      rate: 4.8),
+  Food(
+      id: 3,
+      picturePath:
+          "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+      name: "Sate Padang",
+      description:
+          "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+      ingredients:
+          "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+      price: 200000,
+      rate: 4.8),
+  Food(
+      id: 4,
+      picturePath:
+          "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+      name: "Sate Padang",
+      description:
+          "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+      ingredients:
+          "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+      price: 200000,
+      rate: 4.8),
+  Food(
+      id: 5,
+      picturePath:
+          "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+      name: "Sate Padang",
+      description:
+          "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+      ingredients:
+          "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+      price: 200000,
+      rate: 4.8),
+];
