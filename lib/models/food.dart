dbfcf9b1db527b15280de25db7a84a5de924a13f
montohang <harmonsitohang17@gmail.com>
2021-01-07 01:31:09 +0700

    [MODIFY food card]

---
+++ lib/models/food.dart
@@ -0,0 +1,31 @@
+part of 'models.dart';
+
+class Food {
+  final int id;
+  final String picturePath;
+  final String name;
+  final String description;
+  final String ingredients;
+  final int price;
+  final double rate;
+
+  Food(
+      {this.id,
+      this.picturePath,
+      this.name,
+      this.description,
+      this.ingredients,
+      this.price,
+      this.rate});
+}
+
+Food mockFood = Food(
+    id: 1,
+    picturePath: "https://ajheriscom.files.wordpress.com/2018/08/img_78772.jpg",
+    name: "Sate Padang",
+    description:
+        "Sate Padang adalah sebutan untuk tiga jenis varian sate di Sumatra Barat, yaitu Sate Padang, Sate Padang Panjang dan Sate Pariaman. Sate Padang memakai bahan daging sapi, lidah, atau jerohan (jantung, usus, dan tetelan) dengan bumbu kuah kacang kental (mirip bubur) ditambah cabai yang banyak sehingga rasanya pedas.",
+    ingredients:
+        "daging sapi, lidah, jerohan, jantung, usus,tetelan, kuah kacang kental, cabai",
+    price: 200000,
+    rate: 4.8);
