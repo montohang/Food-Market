749b8f74b1f214200599cc4414ee37951e6c480d
montohang <harmonsitohang17@gmail.com>
2021-01-07 13:13:26 +0700

    [MODIFY Food Page and Food List Item]

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:supercharged/supercharged.dart';
 
