c3ab75a9efe0f7b29a5dfbb686a0ed205bf93e8b
montohang <harmonsitohang17@gmail.com>
2021-01-07 18:36:42 +0700

    [MODIFY Food Detail Page]

--- lib/ui/pages/food_detail_page.dart
+++ lib/ui/pages/food_detail_page.dart
@@ -32,33 +32,36 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
                     fit: BoxFit.cover)),
           )),
           SafeArea(
+            child:
+                //// Back Button
+                Container(
+              height: 100,
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                      onTap: () {
+                        if (widget.onBackButtonPressed != null) {
+                          widget.onBackButtonPressed();
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(3),
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.black12),
+                        child: Image.asset('assets/back_arrow_white.png'),
+                      ))),
+            ),
+          ),
+          SafeArea(
               child: ListView(
             children: [
-              //// Back Button
-              Container(
-                height: 100,
-                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: GestureDetector(
-                        onTap: () {
-                          if (widget.onBackButtonPressed != null) {
-                            widget.onBackButtonPressed();
-                          }
-                        },
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          width: 30,
-                          height: 30,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.black12),
-                          child: Image.asset('assets/back_arrow_white.png'),
-                        ))),
-              ),
               //// Body
               Container(
-                margin: EdgeInsets.only(top: 180),
+                margin: EdgeInsets.only(top: 280),
                 padding: EdgeInsets.symmetric(vertical: 26, horizontal: 16),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
@@ -66,6 +69,7 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
                         topRight: Radius.circular(20)),
                     color: Colors.white),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -73,9 +77,13 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              widget.transaction.food.name,
-                              style: blackFontSytle2,
+                            SizedBox(
+                              width:
+                                  MediaQuery.of(context).size.width - 32 - 102,
+                              child: Text(
+                                widget.transaction.food.name,
+                                style: blackFontSytle2,
+                              ),
                             ),
                             SizedBox(height: 6),
                             RatingStars(widget.transaction.food.rate)
@@ -127,10 +135,56 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
                           ],
                         )
                       ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 14, 0, 16),
+                      child: Text(
+                        widget.transaction.food.description,
+                        style: greyFontSytle,
+                      ),
+                    ),
+                    Text('Ingredients:', style: blackFontSytle3),
+                    Container(
+                        margin: EdgeInsets.fromLTRB(0, 4, 0, 41),
+                        child: Text(widget.transaction.food.ingredients,
+                            style: greyFontSytle)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Total Price',
+                              style: greyFontSytle.copyWith(fontSize: 13),
+                            ),
+                            Text(
+                                NumberFormat.currency(
+                                        locale: 'id-ID',
+                                        symbol: 'IDR ',
+                                        decimalDigits: 0)
+                                    .format(quantity *
+                                        widget.transaction.food.price),
+                                style: blackFontSytle2.copyWith(fontSize: 18))
+                          ],
+                        ),
+                        SizedBox(
+                            width: 163,
+                            height: 45,
+                            child: RaisedButton(
+                                child: Text('Order Now',
+                                    style: blackFontSytle3.copyWith(
+                                        fontWeight: FontWeight.w500)),
+                                onPressed: () {},
+                                color: mainColor,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8))))
+                      ],
                     )
                   ],
                 ),
-              )
+              ),
             ],
           ))
         ],
